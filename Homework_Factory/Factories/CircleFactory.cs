573a9c0b5a46bee6b04278b649e8f80939aed484
Илья <igorshkov.93@yandex.ru>
2020-04-16 16:58:26 +0300

    Complete

---
+++ Homework_Factory/Factories/CircleFactory.cs
@@ -0,0 +1,13 @@
+﻿using Homework_Factory.GeometryObjects;
+using Homework_Factory.Interfaces;
+
+namespace Homework_Factory.Factories
+{
+    public class CircleFactory : Factory
+    {
+        protected override IGeometryObject FactoryMethod()
+        {
+            return new Circle();
+        }
+    }
+}
