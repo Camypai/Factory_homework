7e8e1b26208b0a61fd00484934000a03d2cbf813
Илья <igorshkov.93@yandex.ru>
2020-04-16 17:00:27 +0300

    Fix for pull request

--- Homework_Factory/Factories/Factory.cs
+++ Homework_Factory/Factories/Factory.cs
@@ -1,15 +1,7 @@
-﻿using Homework_Factory.Interfaces;
-
-namespace Homework_Factory.Factories
+﻿namespace Homework_Factory.Factories
 {
     public abstract class Factory
     {
-        public abstract IGeometryObject FactoryMethod();
-
-        public string CreateGeometryObject()
-        {
-            var geometryObject = FactoryMethod();
-            return geometryObject.WhoAmI();
-        }
+        
     }
 }
