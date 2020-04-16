9b77b0082edc52f1d156052a7910ca23c0c31e6d
Илья <igorshkov.93@yandex.ru>
2020-04-16 16:53:33 +0300

    Init

---
+++ Homework_Factory/Factories/Factory.cs
@@ -0,0 +1,15 @@
+﻿using Homework_Factory.Interfaces;
+
+namespace Homework_Factory.Factories
+{
+    public abstract class Factory
+    {
+        public abstract IGeometryObject FactoryMethod();
+
+        public string CreateGeometryObject()
+        {
+            var geometryObject = FactoryMethod();
+            return geometryObject.WhoAmI();
+        }
+    }
+}
